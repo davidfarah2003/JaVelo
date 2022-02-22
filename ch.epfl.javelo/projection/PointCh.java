d044ddf3a3e5ca5241739fa80f51396a02fcfcc4
davidfarah2003 <davidfarah2003@gmail.com>
2022-02-22 13:17:55 +0100

    Work on PointCh

--- ch.epfl.javelo/projection/PointCh.java
+++ ch.epfl.javelo/projection/PointCh.java
@@ -1,5 +1,26 @@
 package projection;
 
-public record PointCh() {
+import java.util.Vector;
 
+import static java.lang.Math.*;
+
+public record PointCh(double e, double n) {
+    public PointCh{
+        if (e < SwissBounds.MIN_E || e > SwissBounds.MAX_E || n > SwissBounds.MAX_N || n < SwissBounds.MIN_N)
+            throw new IllegalArgumentException();
+    }
+
+    double squaredDistanceTo(PointCh that){
+        return pow(distanceTo(that), 2);
+    }
+
+    double distanceTo(PointCh that){
+        double distanceE = abs(this.e - that.e);
+        double distanceN = abs(this.n - that.n);
+        return sqrt(pow(distanceE, 2) + pow(distanceN, 2));
+    }
+
+    double lon(){
+        return 0.0;
+    }
 }
