83b093068c5a2d3f71023a130d0eb8f43bfa9c77
davidfarah2003 <davidfarah2003@gmail.com>
2022-02-22 09:08:55 +0100

    Fix signatures SwissBounds

--- ch.epfl.javelo/projection/SwissBounds.java
+++ ch.epfl.javelo/projection/SwissBounds.java
@@ -1,19 +1,19 @@
 package projection;
 
-public class SwissBounds {
+public final class SwissBounds {
+    private SwissBounds(){};
 
-        private SwissBounds(){};
+    public static double MIN_E = 2_485_000;
+    public static double MAX_E = 2_834_000;
+    public static double MIN_N = 1_075_000;
+    public static double MAX_N = 1_296_000;
+    public static double WIDTH = MAX_E - MIN_E;
+    public static double HEIGHT = MAX_N - MIN_N;
 
-        private static double MIN_E = 2_485_000;
-        private static double MAX_E = 2_834_000;
-        private static double MIN_N = 1_075_000;
-        private static double MAX_N = 1_296_000;
-        private static double WIDTH = MAX_E - MIN_E;
-        private static double HEIGHT = MAX_N - MIN_N;
-
-        public static boolean containsEN(double e, double n){
-            return (e > MIN_E && e < MAX_E  && n > MIN_N && n < MAX_N);
-        }
+    public static boolean containsEN(double e, double n){
+        return (e > MIN_E && e < MAX_E  && n > MIN_N && n < MAX_N);
     }
+
+}
 
 
