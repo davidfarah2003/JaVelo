f1d6c564c5fe54939cfdcf423a9f35e071a1f1a8
davidfarah2003 <davidfarah2003@gmail.com>
2022-02-27 00:09:09 +0100

    Applied corrections to Bits

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -11,9 +11,11 @@ public final class Bits {
      */
     public static int extractSigned(int value, int start, int length){
         //conditions start<=31 length<= 31-start
-        Preconditions.checkArgument(start <= 31 && length <= (31-start)+1); // Preconditions.checkArgument(start>= 0 && start <= 31 && length <= 32 - start && length >= 0)
-        value =  value << 31-start; //shift left to make start the first bit.  value = value << 31 - (start+length-1)
-        value = value >> length; //shift right (signed) . value >> 31 - (length - 1)
+        Preconditions.checkArgument(start <= 31 && length <= (31-start)+1 && length >= 0);
+        // Preconditions.checkArgument(start>= 0 && start <= 31 && length <= 32 - start && length >= 0)
+        // Wrote it 31-start +1 for it to be more clear why it's 32, length >=0 modification correct.
+        value =  value << 31-start; //shift left to make start the first bit.  value = value << 31 - (start+length-1)/ why?
+        value = value >> 31 - (length - 1); //shift right (signed) . value >> 31 - (length - 1) // You are correct
         return value;
     }
 
@@ -24,9 +26,10 @@ public final class Bits {
      * @return (int) the resulting unsigned bit string
      */
     public static int extractUnsigned(int value, int start, int length){
-        Preconditions.checkArgument(start >= 0 && start <= 31 && length <= (31-start)+1 && length < 32);
-        value =  value << 31-start; //shift left to make start the first bit.  value = value << 31 - (start+length-1)
-        value = value >>> length; //shift right (unsigned).   value >> 31 - (length - 1)
+        Preconditions.checkArgument(start >= 0 && start <= 31
+                && length <= (31-start)+1 && length < 32 && length >= 0);
+        value =  value << 31-start; //shift left to make start the first bit.  value = value << 31 - (start+length-1) / again why?
+        value = value >>> 31 - (length - 1); //shift right (unsigned).   "value >> 31 - (length - 1)" / Correct
         return value;
     }
 
