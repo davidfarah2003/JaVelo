0acf6f90e6f9e178a62ce347cb570acc345f4277
davidfarah2003 <davidfarah2003@gmail.com>
2022-03-04 11:58:59 +0100

    Remove Main

--- src/ch/epfl/javelo/Main.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.javelo;
-
-public class Main {
-    public static void main(String[] args) {
-        int i = 5;
-        int test = Q28_4.ofInt(i);
-        System.out.println(test);
-    }
-}
