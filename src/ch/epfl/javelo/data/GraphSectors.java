3c7a6fed9c54d5b4af806eff673aef7b8b2c9fd4
WesND20 <wesleynanadavies@gmail.com>
2022-03-05 12:30:29 +0100

    GraphSectors (to be tested)

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -1,54 +1,74 @@
 package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.SwissBounds;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This record represents models the 16_384 sectors wh
+ */
 public record GraphSectors (ByteBuffer buffer){
 
 
+    // Constants which enable to access the information stored in the buffer about a sector
     private static final int OFFSET_NODE_ID = 0;
-    private static final int OFFSET_NUMBER_OF_NODES = OFFSET_NODE_ID + 4;
-    private static final int NODE_INT = OFFSET_NUMBER_OF_NODES + 2;
-
-
+    private static final int OFFSET_NUMBER_OF_NODES = OFFSET_NODE_ID + Integer.BYTES; // 4
+    private static final int NODE_INT = OFFSET_NUMBER_OF_NODES + Short.BYTES; // 6
 
     // identite du premier noeud (U32) + nombre de noeuds (U16)
     // Chaque secteur int + short
     // 16 384 secteurs
-    public List<Sector> sectorsInArea(PointCh center, double distance){
-        double upper_left_x =   center.e() + 2_600_000 - 2*distance;
-        double upper_left_y =   center.n() + 1_200_000 +  2*distance;
-        double upper_right_x =  center.e() + 2_600_000+ 2*distance;
-        double upper_right_y =  center.n() + 1_200_000 + 2*distance;
-        double lower_right_x =  center.e() + 2_600_000 + 2*distance;
-        double lower_right_y =  center.n() + 1_200_000 - 2*distance;
-        double lower_left_x =  center.e() + 2_600_000 - 2*distance;
-        double lower_left_y =  center.n() + 1_200_000 - 2*distance;
 
+    /**
+     *
+     * @param center point of interest (PointCh, east-north coordinates)
+     * @param distance (meters) from which a sector is considered in the area of the point of interest
+     * @return a list of sectors in area of the point entered as in input
+     */
+    public List<Sector> sectorsInArea(PointCh center, double distance){
+
+        // getting the coordinates of the summits of the square which defines the range
+        double upper_left_x =   center.e() - SwissBounds.MIN_E - 2*distance; //
+        double upper_left_y =   center.n() - SwissBounds.MIN_N +  2*distance;
+        double lower_right_x =  center.e() - SwissBounds.MIN_E + 2*distance;
+        double lower_right_y =  center.n() - SwissBounds.MIN_N - 2*distance;
+
+
+        // defining some indexes alon the horizontal and vertical axes which will be
+        // needed to compute the actual index of every sector
         int xMin = (int) Math.floor(upper_left_x / 2730);
-        int yMin = (int) Math.floor(lower_left_y / 1730);
-        int xMax = (int) Math.floor(upper_right_x / 2730);
+        int yMin = (int) Math.floor(lower_right_y / 1730);
+        int xMax = (int) Math.floor(lower_right_x / 2730);
         int yMax = (int) Math.floor(upper_left_y / 1730);
 
         ArrayList<Integer> indexes = new ArrayList<>();
 
+        // adding the indexes of all the sectors
+        // which intersect with square centered at the input (PointCh)
         for (int i = xMin; i <= xMax; i++){
             for (int j = yMin; j <= yMax; j++){
                 indexes.add(128 * j + i);
             }
         }
+
         ArrayList<Sector> sectorsInArea = new ArrayList<>();
-
+        // iterating through all the indexes of the sector in the area
         for (int j : indexes){
-            sectorsInArea.add(new Sector(buffer.getInt(NODE_INT * j + OFFSET_NODE_ID), buffer.getShort(NODE_INT * j + OFFSET_NUMBER_OF_NODES)));
+            int startNodeId = buffer.getInt(NODE_INT * j + OFFSET_NODE_ID);
+            // making sure to interpret the short value stored in the buffer as an unsigned int (only positive values)
+            int numberOfNodes =  Short.toUnsignedInt(buffer.getShort(NODE_INT * j + OFFSET_NUMBER_OF_NODES));
+            int endNodeId = startNodeId + numberOfNodes;
+            sectorsInArea.add(new Sector(startNodeId, endNodeId));
         }
-
-        return sectorsInArea;
-
+          return sectorsInArea;
     }
+
+    /**
+     * This nested record represents a sector
+     */
     public record Sector(int startNodeId, int endNodeId){
     }
 }
