7e082718897950c72065ac9955d95a909a58d126
WesND20 <wesleynanadavies@gmail.com>
2022-03-08 21:52:37 +0100

    modified with tests

--- src/ch/epfl/javelo/data/GraphEdges.java
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -116,7 +116,7 @@ public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuff
         }
 
         int lengthIndex = EDGE_INTS*edgeId + OFFSET_LENGTH;
-        int nbSamples = 1 + Math2.ceilDiv(Short.toUnsignedInt(edgesBuffer.getShort(lengthIndex)), Q28_4.ofInt(2));
+        int nbSamples = 1 + Math2.ceilDiv(Q28_4.ofInt(Short.toUnsignedInt(edgesBuffer.getShort(lengthIndex))), Q28_4.ofInt(2));
 
         ArrayList<Float> profileSamples = new ArrayList<>();
         int idFirstSample = Bits.extractUnsigned(profileIds.get(edgeId), 0, 30);
