035c7ecc352dcf8b35ac51595683134d956f49c4
davidfarah2003 <davidfarah2003@gmail.com>
2022-03-15 09:26:40 +0100

    Revision of ElevationProfile

--- src/ch/epfl/javelo/routing/ElevationProfile.java
+++ src/ch/epfl/javelo/routing/ElevationProfile.java
@@ -9,63 +9,56 @@ public final class ElevationProfile {
     private final double length;
     private final float[] elevationSamples;
 
+    private final DoubleSummaryStatistics statistics = new DoubleSummaryStatistics();
+
 
     /**
      * Constructor of the class
-     * @param length
-     * @param elevationSamples
+     * @param length length of the edge
+     * @param elevationSamples y coordinates of the samples
      */
     public ElevationProfile(double length, float[] elevationSamples) {
         Preconditions.checkArgument(length > 0 && elevationSamples.length >= 2);
         this.length = length;
         this.elevationSamples = elevationSamples;
+
+        for (float number : elevationSamples){
+            statistics.accept(number);
+        }
     }
 
 
     /**
-     *
      * @return the length of the profile (meters)
      */
     public double length(){
         return length;
     }
 
-    /**
-     *
-     * @return the statistics of the profile with altitudes
-     */
-    private DoubleSummaryStatistics statistics(){
-        DoubleSummaryStatistics s = new DoubleSummaryStatistics();
-        for (float number : elevationSamples){
-            s.accept(number);
-        }
-        return s;
-    }
 
     /**
-     *
      * @return the minimum value among the altitudes
      */
     public double minElevation(){
-       return statistics().getMin();
+       return statistics.getMin();
     }
 
     /**
-     *
      * @return the maximum value among the altitudes
      */
     public double maxElevation(){
-        return statistics().getMax();
+        return statistics.getMax();
     }
 
     /**
-     *
      * @return the sum of all positive differences between a sample and its predecessor.
      */
     public double totalAscent(){
-        double ascent = 0.0;
+        double ascent = 0;
+        double difference;
+
         for (int i = 0; i < elevationSamples.length - 1; i++){
-            double difference = elevationSamples[i+1] - elevationSamples[i];
+            difference = elevationSamples[i+1] - elevationSamples[i];
             if (difference > 0){
                 ascent +=  difference;
             }
@@ -74,7 +67,6 @@ public final class ElevationProfile {
     }
 
     /**
-     *
      * @return the positive value of the sum of all negative differences between a sample and its predecessor.
      */
     public double totalDescent(){
@@ -89,7 +81,6 @@ public final class ElevationProfile {
     }
 
     /**
-     *
      * @param position (in meters)
      * @return the value of the function sampled (class Function) applied for the x-input position.
      */
