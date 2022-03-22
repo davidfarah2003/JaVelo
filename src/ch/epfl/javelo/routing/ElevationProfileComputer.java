b82dd442c2042bc910d56a5368c8f915d74b0c56
davidfarah2003 <davidfarah2003@gmail.com>
2022-03-22 10:02:19 +0100

    Beautify ElevationProfileComputer

--- src/ch/epfl/javelo/routing/ElevationProfileComputer.java
+++ src/ch/epfl/javelo/routing/ElevationProfileComputer.java
@@ -7,20 +7,26 @@ import java.util.Arrays;
 
 public final class ElevationProfileComputer {
     private static float[] profile;
+    private static double intervalLength;
 
     private ElevationProfileComputer() {
     }
 
+    /**
+     *
+     * @param route
+     * @param maxStepLength
+     * @return
+     */
     public static ElevationProfile elevationProfile(Route route, double maxStepLength) {
         int numberOfSamples = 1 + (int) Math.ceil(route.length() / maxStepLength);
-        double intervalLength = route.length() / (numberOfSamples - 1);
+        intervalLength = route.length() / (numberOfSamples - 1);
         profile = new float[numberOfSamples];
         Preconditions.checkArgument(intervalLength <= maxStepLength && intervalLength > 0);
 
-        fillInitialArray();
+        fillInitialArray(route);
         int indexFirstNumber = firstNumberIndex();
 
-        // assigning each sample an elevation equal to 0 if no valid sample exists
         if(indexFirstNumber == profile.length) {
             Arrays.fill(profile, 0);
         }
@@ -33,15 +39,20 @@ public final class ElevationProfileComputer {
         return new ElevationProfile(route.length(), profile);
     }
 
-
-    private static void fillInitialArray(){
-        int indexFirstNumber = 0;
-        while ((indexFirstNumber < profile.length && Float.isNaN(profile[indexFirstNumber]))) {
-            indexFirstNumber++;
+    /**
+     * Fill the array with the initial values from the route
+     * @param route in question
+     */
+    private static void fillInitialArray(Route route) {
+        for (int sample = 0; sample < profile.length; sample++) {
+            profile[sample] = (float) route.elevationAt(sample * intervalLength);
         }
     }
 
 
+    /**
+     * @return the index of the first real number
+     */
     private static int firstNumberIndex(){
         int indexFirstNumber = 0;
         while ((indexFirstNumber < profile.length && Float.isNaN(profile[indexFirstNumber]))) {
@@ -51,6 +62,9 @@ public final class ElevationProfileComputer {
     }
 
 
+    /**
+     * @return the index of the first real number starting from the end of the array
+     */
     private static int fillNanEnd(){
         int indexFirstNumberEnd = profile.length - 1;
         while (Float.isNaN(profile[indexFirstNumberEnd])) {
@@ -61,29 +75,36 @@ public final class ElevationProfileComputer {
     }
 
 
-    private static void fillIntermediateNanValues(int firstNanIndex, int endIndex){
-        int RealNumberAfter;
-        int RealNumberBefore;
+    /**
+     * Fill the remaining NaN values in the array
+     * @param startIndex index where we start on the array
+     * @param endIndex index where we stop on the array
+     */
+    private static void fillIntermediateNanValues(int startIndex, int endIndex){
+        int firstNanIndex = startIndex;
+        int RealNumberAfterIndex;
+        int RealNumberBeforeIndex;
 
         while (firstNanIndex < endIndex) {
             firstNanIndex++;
 
+            //skip index if it's a real number
             if (!Float.isNaN(profile[firstNanIndex])) {
                 continue;
             }
 
-            RealNumberBefore = firstNanIndex - 1;
-            RealNumberAfter = firstNanIndex + 2;
+            RealNumberBeforeIndex = firstNanIndex - 1;
+            RealNumberAfterIndex = firstNanIndex + 2;
 
-            while (Float.isNaN(profile[RealNumberAfter])) {
-                RealNumberAfter++;
+            while (Float.isNaN(profile[RealNumberAfterIndex])) {
+                RealNumberAfterIndex++;
             }
 
-            for(int NanIndex = firstNanIndex + 1; NanIndex < RealNumberAfter; NanIndex++) {
+            for(int NanIndex = firstNanIndex + 1; NanIndex < RealNumberAfterIndex; NanIndex++) {
                 profile[NanIndex] = (float) Math2.interpolate(
-                        profile[RealNumberBefore],
-                        profile[RealNumberAfter],
-                        (double)(NanIndex - RealNumberBefore) / (RealNumberAfter - RealNumberBefore)
+                        profile[RealNumberBeforeIndex],
+                        profile[RealNumberAfterIndex],
+                        (double)(NanIndex - RealNumberBeforeIndex) / (RealNumberAfterIndex - RealNumberBeforeIndex)
                 );
             }
 
