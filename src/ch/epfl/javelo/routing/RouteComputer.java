d84885e1ecd2466ef28757df6a3ac0db4175c8e2
davidfarah2003 <davidfarah2003@gmail.com>
2022-03-29 15:57:46 +0200

    update test for performance (3 seconds on my machine)

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -14,7 +14,6 @@ public final class RouteComputer {
      * @param graph
      * @param costFunction
      */
-
     public RouteComputer(Graph graph, CostFunction costFunction) {
         this.graph = graph;
         this.costFunction = costFunction;
@@ -28,16 +27,6 @@ public final class RouteComputer {
      */
     public Route bestRouteBetween(int startNodeId, int endNodeId) {
         Preconditions.checkArgument(startNodeId != endNodeId);
-
-        // inner record to represent a node, implementing the comparable interface to let the
-        // PriorityQueue remove method know which element is lowest  in the queue.
-        record WeightedNode(int nodeId, float distance)
-                implements Comparable<WeightedNode> {
-            @Override
-            public int compareTo(WeightedNode that) {
-                return Float.compare(this.distance, that.distance);
-            }
-        }
 
         float[] distance = new float[graph.nodeCount()];
         int[] predecessor = new int[graph.nodeCount()];
@@ -118,82 +107,16 @@ public final class RouteComputer {
         }
 
         return new SingleRoute(edges);
-        /*
-        Preconditions.checkArgument(startNodeId != endNodeId);
-        float[] distance = new float[graph.nodeCount()];
-        int[] predecessor = new int[graph.nodeCount()];
-        Arrays.fill(distance, Float.POSITIVE_INFINITY);
-        Arrays.fill(predecessor, 0);
-        distance[startNodeId] = 0;
-        List<Integer> nodeIdsExplored = new ArrayList<>();
-        nodeIdsExplored.add(startNodeId);
-
-        float minDistanceFirstIdExplored;
-        int id = 0;
-        int idEdge;
-        int endNodeIdEdge;
-
-        float d;
-
-        while (!nodeIdsExplored.isEmpty()) {
-            minDistanceFirstIdExplored = Float.POSITIVE_INFINITY;
-            for (int idExplored : nodeIdsExplored) {
-                if (distance[idExplored] < minDistanceFirstIdExplored) {
-                    id = idExplored;
-                    minDistanceFirstIdExplored = distance[id];
-                }
-            }
-                nodeIdsExplored.remove(new Integer(id));
-
-                if (id == endNodeId) {
-                    break;
-                }
-
-                for (int i = 0; i < graph.nodeOutDegree(id); i++) {
-                    idEdge = graph.nodeOutEdgeId(id, i);
-                    endNodeIdEdge = graph.edgeTargetNodeId(idEdge);
-                    d = distance[id] + (float) (graph.edgeLength(idEdge) * costFunction.costFactor(id, idEdge));
-
-                    if (d < distance[endNodeIdEdge]) {
-                        distance[endNodeIdEdge] = d;
-                        predecessor[endNodeIdEdge] = id;
-                        nodeIdsExplored.add(endNodeIdEdge);
-                    }
-                }
-            }
-
-        List<Integer> finalIds = new ArrayList<>();
-        int i = endNodeId;
-        while (i != startNodeId){
-            finalIds.add(i);
-            i = predecessor[i];
-        }
-        finalIds.add(startNodeId);
-        Collections.reverse(finalIds);
-
-        List<Edge> edges = new ArrayList<>();
-
-        int s;
-        int e;
-        for (int k = 0; k < finalIds.size() - 1; k++){
-            s = finalIds.get(k);
-            e = finalIds.get(k+1);
-            for (int l = 0; l < graph.nodeOutDegree(s); l++){
-                if (graph.edgeTargetNodeId(graph.nodeOutEdgeId(s,l)) == e){
-                    edges.add(Edge.of(graph,graph.nodeOutEdgeId(s,l),s,e));
-                    break;
-                }
-            }
-
-        }
-
-        return new SingleRoute(edges);
 
     }
-}
 
-
-
-         */
+    // inner record to represent a node, implementing the comparable interface to let the
+    // PriorityQueue remove method know which element is lowest  in the queue.
+    record WeightedNode(int nodeId, float distance)
+            implements Comparable<WeightedNode> {
+        @Override
+        public int compareTo(WeightedNode that) {
+            return Float.compare(this.distance, that.distance);
+        }
     }
 }
