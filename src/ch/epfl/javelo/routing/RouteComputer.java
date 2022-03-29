9328595e75214d379127586638d5b2d4564580ea
davidfarah2003 <davidfarah2003@gmail.com>
2022-03-29 16:13:12 +0200

    RouteComputer create other method (primitive one)

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -46,16 +46,15 @@ public final class RouteComputer {
         // Dijkstra's Algorithm
         while (!nodesExplored.isEmpty()) {
 
-            //
             do {
                 nodeChosen = nodesExplored.remove();
-            } while (distance[nodeChosen.nodeId] == Float.NEGATIVE_INFINITY);
+            }
+            while (distance[nodeChosen.nodeId] == Float.NEGATIVE_INFINITY);
+
 
             nodeChosenId = nodeChosen.nodeId;
 
-            if (nodeChosenId == endNodeId) {
-                break;
-            }
+            if (nodeChosenId == endNodeId) break;
 
             for (int i = 0; i < graph.nodeOutDegree(nodeChosenId); i++) {
                 edgeId = graph.nodeOutEdgeId(nodeChosenId, i);
@@ -91,6 +90,7 @@ public final class RouteComputer {
 
         List<Edge> edges = new ArrayList<>();
 
+
         int s;
         int e;
         // getting all the corresponding edges
@@ -105,10 +105,82 @@ public final class RouteComputer {
             }
 
         }
+        return new SingleRoute(edges);
+    }
+
+
+    public Route bestRouteBetweenOld(int startNodeId, int endNodeId){
+        Preconditions.checkArgument(startNodeId != endNodeId);
+        float[] distance = new float[graph.nodeCount()];
+        int[] predecessor = new int[graph.nodeCount()];
+        Arrays.fill(distance, Float.POSITIVE_INFINITY);
+        Arrays.fill(predecessor, 0);
+        distance[startNodeId] = 0;
+        List<Integer> nodeIdsExplored = new ArrayList<>();
+        nodeIdsExplored.add(startNodeId);
+
+        float minDistanceFirstIdExplored;
+        int id = 0;
+        int idEdge;
+        int endNodeIdEdge;
+
+        float d;
+
+        while (!nodeIdsExplored.isEmpty()) {
+            minDistanceFirstIdExplored = Float.POSITIVE_INFINITY;
+            for (int idExplored : nodeIdsExplored) {
+                if (distance[idExplored] < minDistanceFirstIdExplored) {
+                    id = idExplored;
+                    minDistanceFirstIdExplored = distance[id];
+                }
+            }
+            nodeIdsExplored.remove(new Integer(id));
+
+            if (id == endNodeId) {
+                break;
+            }
+
+            for (int i = 0; i < graph.nodeOutDegree(id); i++) {
+                idEdge = graph.nodeOutEdgeId(id, i);
+                endNodeIdEdge = graph.edgeTargetNodeId(idEdge);
+                d = distance[id] + (float) (graph.edgeLength(idEdge) * costFunction.costFactor(id, idEdge));
+
+                if (d < distance[endNodeIdEdge]) {
+                    distance[endNodeIdEdge] = d;
+                    predecessor[endNodeIdEdge] = id;
+                    nodeIdsExplored.add(endNodeIdEdge);
+                }
+            }
+        }
+
+        List<Integer> finalIds = new ArrayList<>();
+        int i = endNodeId;
+        while (i != startNodeId){
+            finalIds.add(i);
+            i = predecessor[i];
+        }
+        finalIds.add(startNodeId);
+        Collections.reverse(finalIds);
+
+        List<Edge> edges = new ArrayList<>();
+
+        int s;
+        int e;
+        for (int k = 0; k < finalIds.size() - 1; k++){
+            s = finalIds.get(k);
+            e = finalIds.get(k+1);
+            for (int l = 0; l < graph.nodeOutDegree(s); l++){
+                if (graph.edgeTargetNodeId(graph.nodeOutEdgeId(s,l)) == e){
+                    edges.add(Edge.of(graph,graph.nodeOutEdgeId(s,l),s,e));
+                    break;
+                }
+            }
+
+        }
 
         return new SingleRoute(edges);
-
     }
+
 
     // inner record to represent a node, implementing the comparable interface to let the
     // PriorityQueue remove method know which element is lowest  in the queue.
