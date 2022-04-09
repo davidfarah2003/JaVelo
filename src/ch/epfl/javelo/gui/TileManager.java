6226f6b6a441441631a72d06dcf9c492e4785654
davidfarah2003 <davidfarah2003@gmail.com>
2022-04-09 17:36:11 +0200

    change test

--- src/ch/epfl/javelo/gui/TileManager.java
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -59,13 +59,6 @@ public final class TileManager {
             }
         }
     }
-    private Path combineTwoPaths(String path1, String path2)
-    {
-
-        File file1 = new File(path1);
-        File file2 = new File(file1, path2);
-        return file2.toPath();
-    }
 
 
     record TileId(int zoomLevel, int xIndex, int yIndex) {
--- test/ch/epfl/javelo/gui/TestTileManager.java
+++ test/ch/epfl/javelo/gui/TestTileManager.java
@@ -14,14 +14,10 @@ public final class TestTileManager extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TileManager tm = new TileManager(
-                Path.of("./cache"), "tile.openstreetmap.org");
-        Image tileImage = tm.getTileImage(
-                new TileManager.TileId(19, 271725, 185422)
-        );
-        Image image2 = tm.getTileImage(
-                new TileManager.TileId(19, 271725, 185422)
-        );
+        TileManager tm = new TileManager(Path.of("./cache"), "tile.openstreetmap.org");
+
+        Image tileImage = tm.getTileImage(new TileManager.TileId(19, 271725, 185422));
+        Image image2 = tm.getTileImage(new TileManager.TileId(19, 271725, 185422));
 
         assertEquals(tileImage.getHeight(), image2.getHeight());
         Platform.exit();
